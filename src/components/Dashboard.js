79dd92943f03f50d57078f0448f0111a80f2a3b2
Cxpycat <87539788+Cxpycat@users.noreply.github.com>
2021-07-17 03:53:54 +0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,15 +4,15 @@ import Task from './Task';
 
 
 class Dashboard extends React.Component{
-    handleClick = () =>{
-        const {handleLoginClick} =this.props;
-        handleLoginClick(false);
-        }
+        handleClick = () =>{
+            const {handleLoginClick} =this.props;
+            handleLoginClick(false);
+            }
 
-  state ={
-    todos: [],
-    error: ''
- }
+      state ={
+        todos: [],
+        error: ''
+    }
 
  componentDidMount = async() => {
    const urlToDo =`https://jsonplaceholder.typicode.com/todos?userId=${this.props.userId}`;
@@ -28,14 +28,16 @@ try{
     })
   } 
 } 
-toggleCompleted = id => {
-  const todos = [...this.state.todos];
-  const todo = todos.filter(todo => todo.id === id)[0];
-  const index = todos.map(function(e) { return e.id; }).indexOf(id);
-  todo.completed = !todo.completed;
-  todos[index] = todo;
-  this.setState({todos});
-};
+  changeToDo = id => {
+    const todos = [...this.state.todos];
+    const todo = todos.filter(todo => todo.id === id)[0];
+    const index = todos.map(function(temp) { return temp.id; }).indexOf(id);
+    todo.completed = !todo.completed;
+    todos[index] = todo;
+    this.setState({
+    todos
+    });
+  };
 
 
   render(){
@@ -47,7 +49,7 @@ toggleCompleted = id => {
       {todos.map(task =>(<Task 
       task={task}
       key={task.id}
-      toggleCompleted={() => this.toggleCompleted(task.id)}
+      toggleCompleted={() => this.changeToDo(task.id)}
      ></Task>
        )) 
      }<br></br>
